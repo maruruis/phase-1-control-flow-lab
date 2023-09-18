19b994707b80e7424624a12e5726811d3f7c6840
Ian Kimani <ianmwng@gmail.com>
2023-09-18 21:13:14 +0300

    complete assingment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x) {
   // Write your code here!
-}
+  if(x<=400) {
+    return 'This one is on me!'
+    } else if (x > 2000 && x <=2500) {
+      return 'I will gladly take your thirty bucks.'
+    } else {
+      return 'No can do.'
+    }
+  }
+const y= 'NYC'
+function ternaryCheckCity(y){
+  return (y ==='NYC') ? `Ok, sounds good.`: `No go.`;
 
-function ternaryCheckCity(){
-  // Write your code here!
+  
 }
+const z= 'generous'
+function switchOnCharmFromTip(z){
+  switch (z){
+    case 'generous':
+      return ('Thank you so much.');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+      case 'not as generous':
+        return('Thank you.');
+        default:
+
+        return ('Bye.')
+      
+  }
+  
 }
